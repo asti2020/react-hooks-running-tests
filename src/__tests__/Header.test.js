44d5412dcc0e41bc8e9df9ca33e74dac714adc32
Astika Luintel <astild2020@gmail.com>
2022-09-18 20:29:22 -0400

    commit tree 4a6e3463d7021fd9ee4d2745cbbff2d989c24100

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,19 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import React from "react";
+
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
 
 import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
-
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  let element = screen.queryByText("hello from the Header!")
+  screen.debug(element);
+  expect(element).toBeInTheDocument();
 });
